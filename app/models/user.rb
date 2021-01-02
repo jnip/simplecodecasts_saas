9a564b2805d4b8324f4b39466de14863cdd7cd2f
Joe Nip <joe-github@outlook.com>
2021-01-02 13:28:23 -0400

    Modified devise for stripe. Updated routes file with new devise controller. Added connection for Plan to Stripe’s product_id.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   belongs_to :plan
+  attr_accessor :stripe_card_token
+
+  def save_with_payment
+    if valid?
+      customer = Stripe::Customer.create(description: email, plan: Plan.find(plan_id).stripe_id, card: stripe_card_token)
+      self.stripe_customer_token = customer.id
+      save!
+    end
+  end
 end
---
+++ db/migrate/20210102164615_add_stripe_id_to_plan.rb
@@ -0,0 +1,5 @@
+class AddStripeIdToPlan < ActiveRecord::Migration[6.1]
+  def change
+    add_column :plans, :stripe_id, :string
+  end
+end
